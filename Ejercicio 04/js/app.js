3e48f7bbff1fb41fdd41c37967bb55b87a4c88b2
ecreyes <ereyesoyarzo@gmail.com>
2018-09-07 19:08:15 -0300

    tabla y petición http

--- Ejercicio 04/js/app.js
+++ Ejercicio 04/js/app.js
@@ -2,8 +2,9 @@ var app = angular.module('myApp',[]);
 
 app.controller('mainCtrl',['$scope','$http',function($scope,$http){
   $scope.profesores = {};
-  $http.get('json/profesores.json').then(function(data){
-    console.log(data);
+  $http.get('json/profesores.json').then(function(resultado){
+    //console.log(resultado.data.profesores);
+    $scope.profesores = resultado.data.profesores;
   });
 
 }]);
